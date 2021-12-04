424bd7c9681eeba7ec168b9ce4f2632ccb5af9c5
Your Name <m89040885558@yandex.ru>
2021-12-04 20:47:08 +0300

    add Lesson 1,2

---
+++ Lesson 1/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd "Lesson 1/Homework"
+mkdir s_1 s_2 s_3
+cd "/d/QA 2021/Lesson 1/Homework/s_3"
+touch name1.txt name2.json name3.json name4.json name2.txt
+mkdir -p q_1/q_1/q_2/q_3 
+ls
+mv name1.txt name2.json q_1/q_1
